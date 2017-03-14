5aefdb8d91822c6ab7e87d2a4f813ead2228ab63
nomisum <nomisum@googlemail.com>
2017-03-14 13:12:49 +0100

    removed old stuff, preparations for presets

--- public/plugin.js
+++ public/plugin.js
@@ -205,44 +205,56 @@ require([
         });
     }
 
-    // mapping for font MapSym-FR-Land
-    function getNATOSymbol(symbolString) {
-        var natoSymbol = "";
-        switch (symbolString) {
-            case "inf": natoSymbol = "+";
-            break;
-            case "motor_inf": natoSymbol = "®";
-            break;
-            case "mech_inf": natoSymbol = "®";
-            break;
-            case "armor": natoSymbol = "A";
-            break;
-            case "recon": natoSymbol = "Û";
-            break;
-            case "air": natoSymbol = "p";
-            break;
-            case "plane": natoSymbol = "w";
-            break;
-            case "uav": natoSymbol = "g";
-            break;
-            case "med": natoSymbol = "Â";
-            break;
-            case "art": natoSymbol = "#";
-            break;
-            case "mortar": natoSymbol = "Ó";
-            break;
-            case "hq": natoSymbol = "¬0";
-            break;
-            case "support": natoSymbol = "B";
-            break;
-            case "maint": natoSymbol = "<";
-            break;
-            case "service": natoSymbol = "";
-            break;
-            default: natoSymbol = "";
-        }
-        return natoSymbol;
+    /*
+    addInsertPresetClickEvent(document.querySelector('.tag'),document.getElementbyID('match-definition'));
+
+    */
+
+    /* todo: get correct text from xml file */
+    function addInsertPresetClickEvent(button, inputField) {
+        document.getElementById(button).addEventListener('click', function () {
+            var textField = document.getElementById(inputField);
+            insertTextAtCursorPosition('textzeug', textField);
+        });
     }
+
+    function insertTextAtCursorPosition(text, inputField) {
+      var input = inputField;
+      console.log(input);
+      if (input == undefined) { return; }
+      var scrollPos = input.scrollTop;
+      var pos = 0;
+      var browser = ((input.selectionStart || input.selectionStart == "0") ? 
+        "ff" : (document.selection ? "ie" : false ) );
+      if (browser == "ie") { 
+        input.focus();
+        var range = document.selection.createRange();
+        range.moveStart ("character", -input.value.length);
+        pos = range.text.length;
+      }
+      else if (browser == "ff") { pos = input.selectionStart };
+
+      var front = (input.value).substring(0, pos);  
+      var back = (input.value).substring(pos, input.value.length); 
+      input.value = front+text+back;
+      pos = pos + text.length;
+      if (browser == "ie") { 
+        input.focus();
+        var range = document.selection.createRange();
+        range.moveStart ("character", -input.value.length);
+        range.moveStart ("character", pos);
+        range.moveEnd ("character", 0);
+        range.select();
+      }
+      else if (browser == "ff") {
+        input.selectionStart = pos;
+        input.selectionEnd = pos;
+        input.focus();
+      }
+      input.scrollTop = scrollPos;
+    }
+
+
 
 
     // cb = callback
