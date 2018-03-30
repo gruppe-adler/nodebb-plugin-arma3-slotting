46e334cdfaab73f5423beeac58460f522f8bc33f
Christian Klemm <chris579@web.de>
2018-03-30 14:37:18 +0200

    PostMessage fix

--- public/plugin.js
+++ public/plugin.js
@@ -43,7 +43,7 @@ require([
                     {
                         type: 'bootboxConfirmResult',
                         data: result
-                    });
+                    }, '*');
                 });
             } break;
         }
